1b8eb65bee6483982b7caf800696d8e6d7b2c3fd
Kevin Zhang <kevin.zqw@gmail.com>
2017-04-20 09:04:08 +0800

    almost done

--- Agatha.Christie.Text/PyTool/HTMLGenerator.py
+++ Agatha.Christie.Text/PyTool/HTMLGenerator.py
@@ -15,14 +15,22 @@ output_dir = os.path.join(base_dir, 'output')
 process_file = os.path.join(base_dir, book_dir_name, book_file_name)
 html_template_file = os.path.join(base_dir, r'Templates', r'base_html.xhtml')
 
+duokan_comment_ref_template = '<sup><a class="duokan-footnote" href="#footnote{}" id="note{}"><img alt="注" src="../Images/note.png"/></a></sup>'
+duokan_comment_define_template = '    <li class="duokan-footnote-item" id="footnote{}"><a href="#note{}">{}</a></li>'
+duokan_comment_start = '<ol class="duokan-footnote-content hr"></ol>\n\n  <ol class="duokan-footnote-content">'
+duokan_comment_end = '</ol>'
+
 page_break = '【PAGE_BREAK】'
 pattern_chapter_no_name = r'【CHAPTER_NO:(\d+)】【CHAPTER_NAME:(.+)】'
-pattern_duokan_comment = r'【DUOKAN_COMMENT\:.*?】'
+pattern_duokan_comment = r'(【DUOKAN_COMMENT\:(.*?)】)'
 
 replace_chapter_no = r'【CHAPTER_NO】'
 replace_chapter_name = r'【CHAPTER_NAME】'
 replace_chapter_text = r'【CHAPTER_TEXT】'
-replace_duokan_comment = r'【DUOKAN_COMMENT】'
+
+replace_duokan_comment_start = r'【DUOKAN_COMMENT_START】'
+replace_duokan_comment_end = r'【DUOKAN_COMMENT_END】'
+replace_duokan_comment_list = r'【DUOKAN_COMMENT】'
 
 
 def get_chapter_file_name(chapter_no):
@@ -34,18 +42,53 @@ def get_base_html():
         return f.read()
 
 
-# return list of duokan comment string
-def process_duokan_comment(chapter):
-    all_comments = re.findall(pattern_duokan_comment, chapter)
-    if len(all_comments) == 0:
-        return []
+def beautiful_lines(chapter_lines):
+    start_index = 0
+    for i in range(len(chapter_lines)):
+        text = chapter_lines[i]
+        if len(text.strip()) == 0:
+            start_index = i
+        else:
+            break
 
-    for comment in all_comments:
-        print(comment)
+    last_index = len(chapter_lines)
+    for i in reversed(range(last_index)):
+        text = chapter_lines[i]
+        if len(text.strip()) == 0:
+            last_index = i
+        else:
+            break
+
+    result_lines = []
+    for text in chapter_lines[start_index:last_index]:
+        result_lines.append('  ' + text)
+
+    return result_lines
 
 
-def save_html_file():
-    print('save')
+def save_html_file(chapter_lines, duokan_comment, chapter_no, chapter_name):
+    chapter_lines = beautiful_lines(chapter_lines)
+    if len(chapter_lines) == 0:
+        return
+
+    html = get_base_html()
+
+    html = html.replace(replace_chapter_no, chapter_no)
+    html = html.replace(replace_chapter_name, chapter_name)
+
+    chapter_text = '\n'.join(chapter_lines)
+    html = html.replace(replace_chapter_text, chapter_text)
+
+    if len(duokan_comment) > 0:
+        comment_text = '\n\n'.join(duokan_comment)
+        html = html.replace(replace_duokan_comment_start, duokan_comment_start)
+        html = html.replace(replace_duokan_comment_list, comment_text)
+        html = html.replace(replace_duokan_comment_end, duokan_comment_end)
+    else:
+        html = html.replace(replace_duokan_comment_start, '')
+        html = html.replace(replace_duokan_comment_list, '')
+        html = html.replace(replace_duokan_comment_end, '')
+    print(html)
 
 
 def all_process():
@@ -54,28 +97,48 @@ def all_process():
 
         chapter_lines = []
         duokan_comment = []
+        comment_index = 1
         chapter_no = ''
         chapter_name = ''
+
         for line in all_lines:
+            line = line.strip()
             chapter_started = len(chapter_lines) > 0
-            empty_line = len(line.strip()) == 0
 
             if page_break in line and chapter_started:
-                save_html_file()
+                save_html_file(chapter_lines, duokan_comment, chapter_no, chapter_name)
+
+                # clear for next chapter
                 chapter_lines.clear()
                 duokan_comment.clear()
+                comment_index = 1
+                chapter_no = ''
+                chapter_name = ''
                 continue
 
-            if empty_line and not chapter_started:
+            if len(line) == 0:
+                if chapter_started:
+                    chapter_lines.append(line)
                 continue
 
             chapter_groups = re.findall(pattern_chapter_no_name, line)
             if len(chapter_groups) > 0:
-                chapter_no = chapter_groups[0][0]
-                chapter_name = chapter_groups[0][1]
+                chapter_no, chapter_name = chapter_groups[0]
                 continue
 
-            print(line)
+            if not line.startswith('<'):
+                line = '<p>' + line + '</p>'
+
+            comment_groups = re.findall(pattern_duokan_comment, line)
+            for comment_tag, comment_text in comment_groups:
+                line = line.replace(comment_tag, duokan_comment_ref_template.format(comment_index, comment_index))
+                duokan_comment.append(duokan_comment_define_template.format(comment_index, comment_index, comment_text))
+                comment_index += 1
+
+            chapter_lines.append(line)
+
+        # save the last chapter
+        save_html_file(chapter_lines, duokan_comment, chapter_no, chapter_name)
 
 
 if __name__ == '__main__':
