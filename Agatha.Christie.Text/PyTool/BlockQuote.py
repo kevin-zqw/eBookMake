80276c99c83b0399e3d6ee8a96103234a76718ab
Kevin Zhang <kevin.zqw@gmail.com>
2017-06-25 23:55:08 +0800

    blockquote脚本

--- Agatha.Christie.Text/PyTool/BlockQuote.py
+++ Agatha.Christie.Text/PyTool/BlockQuote.py
@@ -8,12 +8,31 @@ import sys
 
 
 def process_block_quote(file_path):
-    print(file_path)
+    with open(file_path, 'r+', encoding='utf-8') as f:
+        result_lines = []
+        block_started = False
+        for line in f.readlines():
+            need_process = True
+            if 'bbb' in line or '<blockquote>' in line:
+                block_started = True
+                need_process = False
+            if 'ccc' in line or '</blockquote>' in line:
+                block_started = False
+                need_process = False
+
+            if need_process and block_started:
+                trimmed = line.strip()
+                line = '<p>{}</p>\n'.format(trimmed)
+
+            result_lines.append(line)
+
+
+        f.seek(0)
+        f.write(''.join(result_lines))
+        f.truncate()
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        return
-
-    file_path = sys.argv[1]
-    process_block_quote(file_path)
+    if len(sys.argv) >= 2:
+        file_path = sys.argv[1]
+        process_block_quote(file_path)
