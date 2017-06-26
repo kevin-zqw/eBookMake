d65e02825d06a09482767f3f40a1d74e903ecd2b
Kevin Zhang <kevin.zqw@gmail.com>
2017-06-26 11:52:19 +0800

    352

--- Agatha.Christie.Text/PyTool/BlockQuote.py
+++ Agatha.Christie.Text/PyTool/BlockQuote.py
@@ -8,21 +8,29 @@ import sys
 
 
 def process_block_quote(file_path):
+    custom_start = 'bbb'
+    custom_end = 'ccc'
+    start_tag = '<blockquote>'
+    end_tag = '</blockquote>'
     with open(file_path, 'r+', encoding='utf-8') as f:
         result_lines = []
         block_started = False
         for line in f.readlines():
-            need_process = True
-            if 'bbb' in line or '<blockquote>' in line:
+            is_block_tag = False
+            if custom_start in line or start_tag in line:
                 block_started = True
-                need_process = False
-            if 'ccc' in line or '</blockquote>' in line:
+                is_block_tag = True
+            if custom_end in line or end_tag in line:
                 block_started = False
-                need_process = False
+                is_block_tag = True
 
-            if need_process and block_started:
+            if block_started and not is_block_tag:
                 trimmed = line.strip()
                 line = '<p>{}</p>\n'.format(trimmed)
+
+            if is_block_tag:
+                line = line.replace(custom_start, start_tag)
+                line = line.replace(custom_end, end_tag)
 
             result_lines.append(line)
 
