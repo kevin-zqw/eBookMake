468410a16d6be33d478c2d6e5f6ade692f40d1ce
Kevin Zhang <kevin.zqw@gmail.com>
2017-07-25 13:39:45 +0800

    script

---
+++ Agatha.Christie.Text/PyTool/gbk2utf8.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+__author__ = 'kevin'
+
+import os
+import shutil
+import re
+import sys
+
+if __name__ == '__main__':
+    base_dir = '/Users/kevin/GitHub/eBookMake/zuihoudediqiuzhanshen'
+
+    for filename in os.listdir(base_dir):
+        if not filename.endswith('html'):
+            continue
+
+        file_path = os.path.join(base_dir, filename)
+        text = ''
+        with open(file_path, 'r', encoding='gbk') as f:
+            text = f.read()
+
+        with open(file_path, 'w', encoding='utf-8') as f:
+            f.write(text)
+        
